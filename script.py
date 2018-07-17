f68d474c11ce698a0488efc631b9745729b0e4b5
Omkar Vijay <omkarvijay5@gmail.com>
2018-07-17 14:19:49 +0530

    write last lines as well

--- script.py
+++ script.py
@@ -66,6 +66,7 @@ class FormatIndex(object):
                         print line
                 else:
                     self.nested_indices.append(line)
+        self.write_nested_indexes()
 
 
 def main():
